d89a8e227570f6da48a3c5932a21efd4281fe86e
mcdyzg <940608110@qq.com>
2018-02-02 16:26:35 +0800

    增加一种懒加载方式

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,15 @@ import Router from 'vue-router'
 // import todo from '@/pages/todo'
 // import TestVuex from '@/pages/TestVuex'
 
-const Hello = resolve => require(['@/pages/Hello/Hello.vue'], resolve)
-const todo = resolve => require(['@/pages/todo/todo.vue'], resolve)
-const TestVuex = resolve => require(['@/pages/TestVuex/TestVuex.vue'], resolve)
+// 异步加载方法一：
+// const Hello = resolve => require(['@/pages/Hello/Hello.vue'], resolve)
+// const todo = resolve => require(['@/pages/todo/todo.vue'], resolve)
+// const TestVuex = resolve => require(['@/pages/TestVuex/TestVuex.vue'], resolve)
 
+// 异步加载方法二：
+const Hello = () => import('@/pages/Hello/Hello.vue')
+const todo = () => import('@/pages/todo/todo.vue')
+const TestVuex = () => import('@/pages/TestVuex/TestVuex.vue')
 
 Vue.use(Router)
 
